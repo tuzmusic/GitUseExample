1165db166f45324c94abdd23517862f5c03b2710
tuzmusic <tuzmusic@gmail.com>
2018-05-20 13:55:20 -0400

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Jonathan Tuzman on 5/20/18.
+//  Copyright © 2018 Jonathan Tuzman. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
